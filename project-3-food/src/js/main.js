e5442271762312411f51e06906de42fab944c226
Al Tsaplin <al.tsaplin@gmail.com>
2023-10-12 08:15:45 +0300

    Upd(project "food") Adding product cards... ... dynamically with JS-classes.

--- project-3-food/src/js/main.js
+++ project-3-food/src/js/main.js
@@ -1,7 +1,9 @@
 import tabsFunc from './modules/tabs.js';
 import timerFunc from './modules/timer.js';
 import modalFunc from './modules/modal.js';
+import classesFunc from './modules/classes.js';
 
 tabsFunc();
 timerFunc();
 modalFunc();
+classesFunc();
