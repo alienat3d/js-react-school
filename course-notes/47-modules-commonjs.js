cbd482badf0197b38c3ff7175053603c6637695a
Al Tsaplin <al.tsaplin@gmail.com>
2023-11-18 03:03:18 +0300

    Add (notes) About modular Javascript concepts and self-executing anonymous functions

---
+++ course-notes/47-modules-commonjs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// * === Модульный Javascript & CommonJS === * \\
+
+// ? 
+
+
+// |===:===:===:===>
+/** links:
+ * https://largescalejs.ru/commonjs-modules/
+ * https://webpack.js.org/configuration/devtool/
+ * 
+ * */
