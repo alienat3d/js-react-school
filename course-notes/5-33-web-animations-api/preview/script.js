c37d00a29cfdb931885caa3bdef30eb6e3ae9e61
Al Tsaplin <al.tsaplin@gmail.com>
2023-11-24 07:58:21 +0300

    Add (notes & examples) JS-animations With setInterval & requestAnimationFrame. Comparisons with pro & cons.

---
+++ course-notes/5-33-web-animations-api/preview/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// * === Web Animations API === * \\
+
+const btnPhone = document.querySelector('#iphone'),
+  btnMacbook = document.querySelector('#macbook'),
+  images = document.querySelectorAll('img');
+
+
+  
+// |===:===:===:===>
+/** links:
+ * (Element.animate()): https://developer.mozilla.org/ru/docs/Web/API/Element/animate
+ * */
