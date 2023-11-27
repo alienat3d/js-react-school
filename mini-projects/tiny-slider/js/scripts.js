c0eff438f6d381eeb27ecd85d513c3b5478e284a
Al Tsaplin <al.tsaplin@gmail.com>
2023-11-27 12:28:43 +0300

    Add (notes) Useful JS-libs list

---
+++ mini-projects/tiny-slider/js/scripts.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// ? Docs: https://ganlanyuan.github.io/tiny-slider/
+
+import { tns } from 'tiny-slider';
+
+tns({
+  container: '.my-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true
+});
