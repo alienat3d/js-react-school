2127ef85628a0f203bde6d054fe0faa94493013a
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-04 21:45:06 +0300

    Done (notes & practice) Reconciliation algorithm Or how to use key-props to increase our App’s speed.

--- projects/react-project-2-employees/src/components/app/app.jsx
+++ projects/react-project-2-employees/src/components/app/app.jsx
@@ -6,10 +6,11 @@ import EmployeeAddForm from '../employee-add-form/employee-add-form';
 
 import './app.css';
 // * 1.0.0 Итак, представим, что data нам пришла с сервера и нам нужно на основе неё создать компонент EmployeesList. [далее в employees-list.jsx]
+// * 3.0.0 Укажем id каждому из объектов, чтобы реакт мог их различить алгоритмом реконциляции. (см. react-course-notes\6-129-reconciliation-algorithm.js), далее перейдём в [src\components\employees-list\employees-list.jsx]
 const data = [
-  {name: 'Денис З.', salary: 42000, increase: false},
-  {name: 'Дмитрий Д.', salary: 177000, increase: false},
-  {name: 'Алексей Ц.', salary: 400000, increase: true},
+  {name: 'Денис З.', salary: 42000, increase: false, id: 0},
+  {name: 'Дмитрий Д.', salary: 177000, increase: false, id: 1},
+  {name: 'Алексей Ц.', salary: 400000, increase: true, id: 2},
 ];
 
 function App() {
