9e5c6381d21add35fa670a753132084fdfde2d82
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-04 19:05:33 +0300

    Done (notes & examples) Using React-component props

--- projects/react-project-1/src/comments/index-w-comments-2.js
+++ projects/react-project-1/src/comments/index-w-comments-2.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { Header, Field, Button } from "./App";
+import App from './App-w-comments-1';
+import { Header, Field, Button } from "./App-w-comments-1";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
