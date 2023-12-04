5034fa44a50371560969182dcffd9079449ca760
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-04 11:01:03 +0300

    Done (notes) About React-components Plus some examples 2 ways of creating components with class and function

---
+++ react-course-notes/5-122-getting-known-with-jsx.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// * === Работаем с JSXпрепроцессором ограничения в нём. (122) === * \\
+
+// todo Подробно и с комментариями в [projects/react-project-1/src/index.js]
+
+// |===:===:===:===>
+/** links:
+ * (EN Новая документация JSX): https://react.dev/learn/writing-markup-with-jsx
+ * (RU Базовая документация JSX): https://ru.reactjs.org/docs/introducing-jsx.html
+ * (Блог с той самой записью): https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
+ * (Перевод новой документации): https://reactdev.ru/learn/writing-markup-with-jsx/
+ * (Список всех обновлении React): https://github.com/facebook/react/blob/main/CHANGELOG.md
+ * */
