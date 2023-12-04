ff7734806ab7cfad598d2a49ecca0d3bbc419f57
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-04 11:25:51 +0300

    Done (notes) About "Strict mode"

--- projects/react-project-1/src/index.js
+++ projects/react-project-1/src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+// * 1.0.0 Чтобы использовать "Strict mode" мы можем импортировать его из объекта React, используя деструктуризацию.
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Header, Field, Button } from "./App";
+// import { Header, Field, Button } from "./App";
 
-
+// 1.0.1 Теперь мы можем его использовать, поместив в него наши компоненты.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 );
