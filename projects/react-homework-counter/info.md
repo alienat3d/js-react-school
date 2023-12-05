a9998985a8ad4e6be57abcb754cfd39744b6aef9
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-05 19:14:44 +0300

    Feat (project Employees List) If click on Cookie ... and by click on employees name: will be name colored and star icon appeared.

---
+++ projects/react-homework-counter/info.md
@@ -0,0 +1,3 @@
+131 - Самостоятельное задание на работу с состояниями
+  (задание): https://codepen.io/ihinstone/pen/QWvYePv
+  (решение): https://codepen.io/ihinstone/pen/BaRbBJd
--- projects/react-project-2-employees/src/components/employees-list-item/employees-list-item.jsx
+++ projects/react-project-2-employees/src/components/employees-list-item/employees-list-item.jsx
@@ -1,33 +1,65 @@
-import './employees-list-item.css';
-// * 2.0.0 Теперь сделаем такой функционал, чтобы по клику на кнопке "печенька" цвет инфо о сотруднике окрашивался в оранжевый, а свойство его объекта increase менялось с true на false и наоборот.
-// 2.0.1 Создадим специальную переменную classNames, в которой будет строчка, которая будет содержать все классы, которые находятся в это элементе. А далее у нас будет условие. Если increase в положении true, то мы добавим этому элементу ещё один класс (важно не забыть пробел перед ним для разделения классов).
-const EmployeesListItem = ({name, salary, increase}) => {
-  let classNames = 'list-group-item d-flex justify-content-between';
+import { Component } from 'react';
 
-  if (increase) {
-    classNames += ' increase';
+import './employees-list-item.css';
+// ? [133]
+// * 1.0.0 Реализуем динамическое присвоение премии по клику на кнопке «печенье». Нам потребуется его модифицировать, добавив в него стейт. Идея в том, что когда у нас клик по кнопке «печенье», то в состояние этого компонента запишется, что этому сотруднику дадут премию.
+// ? Пока мы будем работать именно с классическим стейтом в классовых компонентах, поэтому переделаем в классы.
+
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      rise: false
+    }
   }
-  // 2.0.2 А здесь соответственно подставляем вместо классов переменную classNames.
-  return (
-    <li className={classNames}>
-      <span className="list-group-item-label">{name}</span>
-      <input 
-        type="text"
-        className="list-group-item-input"
-        defaultValue={'₽' + salary} />
-      <button 
-        className="btn-cookie btn-sm"
-        type="button">
-        <i className="fas fa-cookie"></i>
-      </button>
-      <button 
-        className="btn-trash btn-sm"
-        type="button">
-        <i className="fas fa-trash"></i>
-      </button>
-      <i className="fas fa-star"></i>
-    </li>
-  );
+  // * Что у нас тут: Итак, т.к. новый стейт зависит от предыдущего, то запишем через коллбэк-функцию. Эта коллбэк-функция примет в себя один аргумент - стейт. Чтобы потом не писать потом state.increase, очень часто берут и деструктуризируют прямо в аргументе. Далее, после стрелки круглые скобки, чтобы не прописывать return и возвращаем изменённый объект из setState(). Там мы установили новое значение increase, противоположное предыдущему (т.к. у нас булево значение).
+  increaseEmployee = () => {
+    this.setState(({increase}) => ({ 
+      increase: !increase 
+    }))
+  }
+  riseEmployee = () => {
+    this.setState(({rise}) => ({ 
+      rise: !rise 
+    }))
+  }
+  // * Также и здесь нам нужно поменять немного логику, т.к. increase должен теперь приходить не из пропсов, а определяться внутри самого компонента.
+  render () {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+
+    let classNames = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+      classNames += ' increase';
+    }
+    if (rise) {
+      classNames += ' like';
+    }
+    return (
+      <li className={classNames}>
+        <span 
+          className="list-group-item-label"
+          onClick={this.riseEmployee}>{name}</span>
+        <input 
+          type="text"
+          className="list-group-item-input"
+          defaultValue={'₽' + salary} />
+        <button 
+          className="btn-cookie btn-sm"
+          type="button"
+          onClick={this.increaseEmployee}>
+          <i className="fas fa-cookie"></i>
+        </button>
+        <button 
+          className="btn-trash btn-sm"
+          type="button">
+          <i className="fas fa-trash"></i>
+        </button>
+        <i className="fas fa-star"></i>
+      </li>
+    );
+  };
 }
 
 export default EmployeesListItem;
