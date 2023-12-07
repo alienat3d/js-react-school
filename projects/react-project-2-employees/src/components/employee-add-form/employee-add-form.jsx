333ef7e8cd11a419ded9bdf27c3bfcf40c2357c2
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-08 00:39:19 +0300

    Feat (add employee form) Min 3 letters for name and no empty salary field allowed.

--- projects/react-project-2-employees/src/components/employee-add-form/employee-add-form.jsx
+++ projects/react-project-2-employees/src/components/employee-add-form/employee-add-form.jsx
@@ -24,6 +24,8 @@ class EmployeeAddForm extends Component {
   }
   onSubmit = (evt) => {
     evt.preventDefault();
+    // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и т.д.
+    if (this.state.name.length < 3 || !this.state.salary) return;
     this.props.onAdd(this.state.name, this.state.salary);
     this.setState({
       name: '',
@@ -35,9 +37,9 @@ class EmployeeAddForm extends Component {
     return (
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
-        <form 
+        <form
           className="add-form d-flex"
-          onSubmit = {this.onSubmit}>
+          onSubmit={this.onSubmit}>
           <input
             className="form-control new-post-label" type="text"
             placeholder="Как его зовут?"
@@ -53,7 +55,7 @@ class EmployeeAddForm extends Component {
           <button
             className="btn btn-outline-light"
             type="submit">
-              Добавить
+            Добавить
           </button>
         </form>
       </div>
