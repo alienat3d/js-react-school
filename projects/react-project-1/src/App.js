0dcf3b0ee8e155edec4af41f3cbbf1021233eb1b
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-09 01:52:09 +0300

    Feat (Eployees List app) Search Rendering only those employees, whos substring fits to typed letters

--- projects/react-project-1/src/App.js
+++ projects/react-project-1/src/App.js
@@ -1,12 +1,6 @@
-// import { Component, Fragment } from 'react';
 import { Component } from 'react';
 
 import './App.css';
-
-// * == Реакт-фрагменты == * \\
-// ? [6.137.1]
-// todo [начало в react-course-notes\6-137-react-fragments.js]
-// * 1.1.1 Итак, сперва импортируем "Fragment" ↑. 
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -28,35 +22,6 @@ class WhoAmI extends Component {
       occupation: evt.target.value
     })
   }
-  // * 1.1.2 А далее мы можем использовать его уже как компонент для замены нашего пустого div’а. Таким образом мы избавились от пустого div'а.
-  // * 1.1.3 Для определённых задач мы бы могли поступить иначе и оставить в render() <div>, а Fragment указать уже ниже в функции App, возвращающей наши компоненты. ↓
-  // render() {
-  //   const { name, surname, link } = this.props;
-  //   const { age, occupation } = this.state;
-  //   return (
-  //     // <Fragment>
-  //     <div className='wrapper'>
-  //       <button onClick={this.nextYear}>
-  //         {this.state.text}
-  //       </button>
-  //       <h2>My name is {name} {surname}. I’m {age} years old.</h2>
-  //       <h3>My occupation is {occupation}.</h3>
-  //       <a href={link}>My LinkedIn Profile</a>
-  //       <form>
-  //         <label htmlFor="occupation">
-  //           Type your occupation
-  //           <input
-  //             type='text'
-  //             id='occupation'
-  //             onChange={(evt) => this.commitInputChanges(evt, 'some color')} />
-  //         </label>
-  //       </form>
-  //     {/* </Fragment> */}
-  //     </div>
-  //   )
-  // };
-  // * 1.2.0 Однако есть и второй способ, который сейчас чаще всего используется в реакт-фрагментах. Здесь нам уже Fragment не нужно импортировать [см. Ln2]. А также в render() не использовать никакие теги — Реакт и так поймёт, что это реакт-фрагмент.
-  // ? При том, что использовать будем в основном второй, но в редких случаях и первый способ может тоже пригодиться. Когда мы создаём какие-то списки, например, при помощи метода map(), то мы тоже можем использовать реакт-фрагменты, т.е. внутри уже формирующегося каждого элемента списка. И вот там, если мы хотим назначить атрибут key для чего-то, мы сможем это сделать для реакт-фрагментов. "React.Fragment key='123'" — пример записи, если вдруг понадобится подобный функционал, к пустым тегом добавить ключи не получится, поэтому мы используем такой синтаксис.
   render() {
     const { name, surname, link } = this.props;
     const { age, occupation } = this.state;
@@ -82,15 +47,6 @@ class WhoAmI extends Component {
   };
 }
 function App() {
-  // return (
-  //   // <div className="App">
-  //   <Fragment>
-  //     <WhoAmI name='Al' surname='Tsaplin' link='https://www.linkedin.com/in/altdev/' />
-  //     <WhoAmI name='Andrey' surname='Teplonogov' link='https://www.linkedin.com/in/antepliy/' />
-  //   {/* </div> */}
-  //   </Fragment>
-  // );
-  // * [1.2.0]
   return (
     <div className="App">
       <WhoAmI name='Al' surname='Tsaplin' link='https://www.linkedin.com/in/altdev/' />
--- projects/react-project-2-employees/src/components/app/app.jsx
+++ projects/react-project-2-employees/src/components/app/app.jsx
@@ -8,6 +8,15 @@ import EmployeeAddForm from '../employee-add-form/employee-add-form';
 
 import './app.css';
 
+// ? [138.1]
+// * 1.0.0 === Реализуем функционал поиска ===
+/* 1.0.1 Сперва разбираем алгоритм, который нам требуется для такого функционала:
+  1) В компоненте поиска у нас будет сохраняться значение, которое пользователь вводит в строку ввода поиска (это будет стейт);
+  2) Это значение мы будем передавать наверх в app.jsx, т.е. сюда, где мы его будем использовать;
+  3) Мы берём значение строки из поиска и по ней отфильтруем данные в массиве data (точнее значения свойства объектов "name", что соответствует ФИО сотрудников);
+  4) После фильтрации у нас останутся только те сотрудники, что нам нужно отобразить и будут переданы дальше на рендер.
+*/
+// 1.0.2 Для начала создадим новый стейт "term", где у нас позже будет значение строки поиска. ↓
 class App extends Component {
   constructor(props) {
     super(props);
@@ -18,10 +27,12 @@ class App extends Component {
         { name: 'Алексей Васильевич Цвайг', salary: 400000, increase: true, rise: true, id: 2 },
         { name: 'Семён Моисеевич Циммерман', salary: 195000, increase: false, rise: false, id: 3 },
         { name: 'Алёна Алексеевна Воробьёва', salary: 105000, increase: true, rise: false, id: 4 }
-      ]
+      ],
+      term: ''
     }
     this.maxId = 4;
   }
+
   deleteItem = (id) => {
     this.setState(({ data }) => {
       return {
@@ -29,6 +40,7 @@ class App extends Component {
       }
     })
   }
+
   addItem = (name, salary) => {
     const newItem = {
       name,
@@ -44,6 +56,7 @@ class App extends Component {
       }
     });
   }
+
   onToggleProp = (id, prop) => {
     this.setState(({data}) => ({
       data: data.map(item => {
@@ -54,9 +67,33 @@ class App extends Component {
       })
     }))
   }
+  // * 1.1.0 Напишем метод поиска. Здесь нам понадобится два аргумента: 1) строчка, по которой будем искать, 2) массив данных, который будем фильтровать.
+  // 1.1.1 Также стоит предусмотреть разные ситуации: когда пользователь удаляет свой ввод (term.length === 0) — мы просто вернём тот массив, в том виде, как он изначально пришёл (return). Ну, а если условие не выполнится, то мы фильтруем наш массив c объектами методом filter(). Нам нужно в каждом из объектов "item" найти свойство name и сравнить его со значением из term. Можно придумать разные алгоритмы поиска, но пока будем искать любое совпадение, а не по первой букве.
+  searchEmployee = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    // 1.1.2 Итак мы возвращаем, используя метод indexOf(), который ищет подстроки (кусочки строки). И если он ничего не находит, то он вернёт "-1", поэтому логично будет записать "> -1". Таким образом мы будем возвращать, только если что-то найдено, а в случае, если ничего найдено не будет, то метод вернёт "-1" и следовательно условие не выполнится и ничего из коллбэка не вернётся. А если выполнится, то вернутся индексы массивов, где была найдена подобная подстрока. Таким образом такое условие нам вернёт массив элементов, которые действительно подойдут под наш поиск.
+    return items.filter(item => item.name.toLowerCase().indexOf(term.toLowerCase()) > -1)
+  }
+  // * 1.1.8 Как и в случае с deleteItem и onToggleProp, чтобы "поднять стейт" из SearchPanel компонента, нам нужно создать метод, назовём его onUpdateSearch(). Аргументом он будет принимать строку "term" и его действием будет установка стейта. Здесь мы не зависим от предыдущего стейта, т.ч. можем просто передать term (term: term сократили до term - это сокр. запись объекта). 
+  // 1.1.9 Теперь этот метод передадим в компонент. ↓
+  onUpdateSearch = (term) => this.setState({term})
+
+  // 1.0.3 И сразу запишем стейты, применим деструктуризацию, т.к. у нас уже два стейта.
+  // 1.1.3 Теперь результаты поиска нам нужно отобразить. Теперь мы хотим отображать не просто data, а сперва их отфильтровать и уже потом отображать.
+  // 1.1.4 Помним, что если у нас в строке поиска ничего не будет, то у нас просто вернётся весь массив данных и отрендерится целиком.
+  // 1.1.5 Создадим ещё одну переменную visibleData, в которую поместим наш метод с массивом объектов с информацией о сотрудниках и значение строки ввода поиска.
+  // 1.1.6 Теперь в проп data передаём уже результат работы метода visibleData, где у нас будет содержатся массив, отфильтрованный по значению строки поиска.
+  // todo [перейдём в search-panel\search-panel.jsx]
+  // [1.1.9] Передавать мы его будем соответственно в SearchPanel. ↓
+  // todo [перейдём в search-panel\search-panel.jsx]
   render() {
-    const employeesCount = this.state.data.length;
-    const increasedCount = this.state.data.filter(item => item.increase).length;
+    const { data, term } = this.state;
+    const employeesCount = data.length;
+    const increasedCount = data.filter(item => item.increase).length;
+    const visibleData = this.searchEmployee(data, term);
+
     return (
       <div className="app">
         <AppInfo
@@ -64,12 +101,13 @@ class App extends Component {
           increasedCount={increasedCount} />
 
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel
+            onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
 
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp} />
 
