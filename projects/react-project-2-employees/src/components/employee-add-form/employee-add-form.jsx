a6fec40faca63cc752ec6d7bac994d776ebf7e9a
Al Tsaplin <al.tsaplin@gmail.com>
2023-12-11 02:34:16 +0300

    Done (notes & examples) Class fields & static methods & props

--- projects/react-project-2-employees/src/components/employee-add-form/employee-add-form.jsx
+++ projects/react-project-2-employees/src/components/employee-add-form/employee-add-form.jsx
@@ -4,15 +4,19 @@ import { Component } from 'react';
 import './employee-add-form.scss';
 
 // ? [6-141] Работа с SASS-стилями.
-
+// ? [6-145] Class fields & static.
+// * 1.1 Вторая фича полей классов — создание любых свойств без конструктора. Теперь мы можем создавать стейты просто, будто мы присваиваем объект переменной state, не создавая её.
+// * 1.2.0 Разберём ещё одну возможность, которую нам даёт ключевое слово static.
+// ? 1.2.1 Сначала вспомним как мы использовали математические функции при помощи объекта Math, например Math.random(). Но теперь, когда мы знаем про JS-классы. Резонный вопрос, если Math это класс, то как же мы сразу применяем к нему метод, не создавая новый экземпляр этого класса ключевым словом new? И вот такие методы и свойства, которые мы можем использовать без создания класса и называются статическими "static". ↓
+// ? 1.2.3 Однако мы можем делать статичные методы, чтобы реализовать такую возможность, рассмотрим на примере нового метода onLog(). ↓
+// ? 1.2.4 Однако class fields позволяют нам создавать не только статические методы, как было и до них, но и свойства. Когда для целого класса должно быть одно свойство. (см. static logged ↓)
 class EmployeeAddForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      salary: ''
-    }
+  
+  state = {
+    name: '',
+    salary: ''
   }
+
   onValueChange = (evt) => {
     if (evt.target.type === 'text') {
       evt.target.value = evt.target.value.replace(/\d/g, '')
@@ -25,6 +29,7 @@ class EmployeeAddForm extends Component {
       })
     }
   }
+
   onSubmit = (evt) => {
     evt.preventDefault();
     // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и т.д.
@@ -35,6 +40,13 @@ class EmployeeAddForm extends Component {
       salary: ''
     })
   }
+
+  static onLog = () => {
+    console.log('logging');
+  }
+
+  static logged = 'on';
+
   render() {
     const { name, salary } = this.state;
     return (
@@ -44,7 +56,7 @@ class EmployeeAddForm extends Component {
           className="add-form d-flex"
           onSubmit={this.onSubmit}>
           <input
-            className="form-control new-post-label" 
+            className="form-control new-post-label"
             tabIndex={7}
             type="text"
             placeholder="Как его зовут?"
@@ -53,7 +65,7 @@ class EmployeeAddForm extends Component {
             onChange={this.onValueChange} />
           <input
             className="form-control new-post-label"
-            tabIndex={8} 
+            tabIndex={8}
             type="number"
             placeholder="Зарплата в ₽?"
             name="salary"
@@ -70,5 +82,9 @@ class EmployeeAddForm extends Component {
     );
   }
 }
+// ? 1.2.2 Но мы не можем просто так взять название нашего классового компонента и вызвать его метод, у нас вылетит ошибка. ↑
+// EmployeeAddForm.onValueChange();
+// EmployeeAddForm.onLog();
+console.log(EmployeeAddForm.logged);
 
 export default EmployeeAddForm;
