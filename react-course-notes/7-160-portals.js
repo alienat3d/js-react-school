01208422a0af3a2f25e780b9ae982da61b0ea705
Al Zaplin <zaplin.dev@gmail.com>
2026-02-14 19:45:51 +0300

    Add:(Code & Notes) For Lesson #160: Portals

---
+++ react-course-notes/7-160-portals.js
@@ -0,0 +1,12 @@
+// * === Порталы (160) === * \\
+
+// Go to [projects/react-project-1/src/comments/160-App.js]
+
+// |===:===:===:===>
+/** links:
+ * (RU Базовая документация): https://ru.reactjs.org/docs/portals.html
+ * (EN Актуальная документация): https://react.dev/reference/react-dom/createPortal
+ * (EN Переиспользуемые порталы): https://medium.com/trabe/reusable-react-portals-17dead20232b
+ * (RU Статья с хорошим примером): https://habr.com/ru/company/smartprogress/blog/306096/
+ * (RU Ещё одна статья по этой теме): https://nuancesprog.ru/p/11039/
+*/
