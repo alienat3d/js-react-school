80f2d886a8fed3c6279765d18ce86aeeac74f754
Al Zaplin <zaplin.dev@gmail.com>
2026-02-16 18:02:25 +0300

    Add:(Notes) For Lesson #161: Bundling and deploy (links)

---
+++ react-course-notes/7-161-bundling-n-deploy.js
@@ -0,0 +1,12 @@
+// * === Бандлинг и выгрузка проекта на сервер (161) === * \\
+
+// Go to []
+
+// |===:===:===:===>
+/** links:
+ * (RU Документация): https://create-react-app.dev/docs/deployment/
+ * (FTP клиент): https://filezilla.ru/
+ * (Free Hosting): https://www.heroku.com/ - на момент тестов затребовал подключение иностранной банковской карты для создания проекта, поэтому испытать пока не удалось
+ * (Free Hosting): https://www.zzz.com.ua/ru - не пробовал
+ * (linkhost): https://link-host.net/billing/pl.php?5442
+*/
