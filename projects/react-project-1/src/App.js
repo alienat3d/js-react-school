549cd6bfa698d6280a6f1d8d865c44bb80053a3d
Al Zaplin <zaplin.dev@gmail.com>
2026-02-27 19:16:45 +0300

    Add:(Code & Notes) For Lesson #172: Explaining some of React 18 hooks «useId» and also the «Concurrent Mode» with examples of usage «useDeferredValue» & «useTransition» hooks on a simple example

--- projects/react-project-1/src/App.js
+++ projects/react-project-1/src/App.js
@@ -1,62 +1,13 @@
 import './App.css';
-import {Container} from 'react-bootstrap';
-import {useState} from 'react';
-// import TestComponent from './TestComponent';
-
-function useInputWithValidation(initialValue) {
-  const [value, setValue] = useState(initialValue);
-
-  const onChange = evt => setValue(evt.target.value);
-
-  const validateInput = () => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(value);
-  };
-
-  return {value, onChange, validateInput};
-}
-
-  const Form = () => {
-  const input = useInputWithValidation('');
-  const textarea = useInputWithValidation('');
-
-  const color = input.validateInput() ? 'text-success' : 'text-danger';
-
-  return (
-    <Container>
-      <form className="w-50 border mt-5 p-3 m-auto">
-        <div className="mb-3">
-          <label htmlFor="output" className="form-label mt-3">Output</label>
-          <input value={`Email: ${input.value} | Note: ${textarea.value}`}
-                 type="text"
-                 className="form-control bg-warning-subtle"
-                 id="output"
-                 disabled/>
-          <label htmlFor="email" className="form-label mt-3">Email address</label>
-          <input onChange={input.onChange}
-                 type="email"
-                 value={input.value}
-                 className={`form-control ${color}`}
-                 id="email"
-                 placeholder="name@example.com"/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="textarea" className="form-label">Note</label>
-          <textarea onChange={textarea.onChange}
-                    value={textarea.value}
-                    className="form-control"
-                    id="textarea"
-                    rows="3"></textarea>
-        </div>
-      </form>
-    </Container>
-  );
-};
+// import FormComponent from './components/FormComponent';
+// import BatchingExample from './components/BatchingExample';
+import ConcurrentModeExample from './components/ConcurrentModeExample';
 
 function App() {
   return (
-    <Form/>
-    // <TestComponent/>
+    <ConcurrentModeExample/>
+    // <FormComponent/>
+    // <BatchingExample/>
   );
 }
 
