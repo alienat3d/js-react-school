78d070159348f48befdf23fe43d5f6d94789756f
Al Zaplin <zaplin.dev@gmail.com>
2026-03-07 20:50:13 +0300

    Add:(Code & Notes) For Lesson #179: Explained usage of the «useReduce» hook

--- projects/mini-projects/react-slider/src/index.js
+++ projects/mini-projects/react-slider/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,7 +6,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <App/>,
+    <StrictMode><App/></StrictMode>,
   document.getElementById('root')
 );
 
