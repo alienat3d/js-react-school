4ff639c8776ff128451a49e3fc72c4da38aebf4b
Al Zaplin <zaplin.dev@gmail.com>
2026-04-05 16:33:11 +0300

    Add:(Code & Notes) For Lesson #194: Connect the Counter component to the Redux store, while examining the 'connect' method in detail

--- projects/mini-projects/redux-playground/src/actions.js
+++ projects/mini-projects/redux-playground/src/actions.js
@@ -1,3 +1,6 @@
 export const inc = () => ({type: 'INC'});
 export const dec = () => ({type: 'DEC'});
-export const rnd = (value) => ({type: 'RND', payload: value});
+// 194.6.1 Теперь мы поместим метод генерации случайного числа сюда в свойство payload.
+// export const rnd = (value) => ({type: 'RND', payload: value});
+// (Go to [/src/components/Counter.js])
+export const rnd = () => ({type: 'RND', payload: Math.floor(Math.random() * 10)});
