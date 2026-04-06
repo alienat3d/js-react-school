c46234cc3d999ae9778eb9d81f2d073545fcf955
Al Zaplin <zaplin.dev@gmail.com>
2026-04-06 15:56:15 +0300

    Add:(Notes) For Lesson #196: A short explanation of what the 'Redux DevTools' extension is and how to use it

--- projects/mini-projects/redux-playground/comments/196-index.js
+++ projects/mini-projects/redux-playground/comments/196-index.js
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/App';
 
+// 196.1 Здесь мы добавим вторым атрибутом такую строчку, как описано в документации к расширению Redux DevTools на Github (см. в ссылках к уроку).
+// (Go to [react-course-notes/8-196-redux-devtools.md])
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const container = document.getElementById('root');
 const root = createRoot(container);
